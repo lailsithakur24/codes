c80dbefe5828ed4cde9fddc51287051ba1673e3b
Lailsi Thakur <lailsithakur24@navgurukul.org>
2025-04-03 13:18:43 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,192 @@
+// let n = 5;
+// for(let i = 0; i <=n; i++){
+//     let str = '';
+//     for(let j = 0 ; j <=i; j++){
+//         str += '*';
+//     }
+//     console.log(str);
+// }
+
+
+
+
+
+// let n = 5;
+// for(let i = 1 ; i <=n; i++){
+//     str ='';
+//  for(let j = 1 ; j <= n-i; j++ ){
+//     str += " "
+//  }
+//  for(let k = 0; k < i ; k++){
+//     str += ' *'
+//  }
+//  console.log(str);
+
+// }
+
+
+
+// let n = 5;
+
+// for (let i = 1; i <= n; i++) {
+//     let str = '';
+
+//     for (let j = 1; j <= i; j++) {
+//         str += '*';
+//     }
+//     for (let j = 0; j < (n - i) * 2; j++) {
+//         str += ' ';
+//     }
+//     for (let j = 1; j <= i; j++) {
+//         str += '*';
+//     }
+
+//     console.log(str);
+// }
+
+
+
+
+// let n = 5;
+// for(let i = 0 ; i < n; i++){
+//     str ='';
+//  for(let j = n; j >=i; j-- ){
+//     str += " ";
+//  }
+//  for(let k = 0; k <= i*2 ; k++){
+//     str += '*';
+//  }
+//  console.log(str);
+
+// }
+
+
+
+
+// let n = 5;
+// for (let i = 0; i <= n; i++) {
+//     let str = '';
+//     for (let j = 0; j < i; j++) {
+//         str += '*';
+//     }
+//     console.log(str);
+// }
+// for(let i = 0; i < n; i++){
+//     let str = '';
+//     for(let j = 0 ; j <n-i-1; j++){
+//         str += '*';
+//     }
+//     console.log(str);
+// }
+
+
+
+
+
+// let n = 5;
+// let even;
+// for (let i = 1; i <= n; i++) {
+//     let pattern = '';
+//     for (let j = 1; j <= i; j++) {
+//         if (i % 2 == 0) {
+//             if (j % 2 == 0) {
+//                 pattern += "1 ";
+//             } else {
+//                 pattern += "0 ";
+//             }
+//         }else{
+            
+//         if (j % 2 == 0) {
+//             pattern += "0 ";
+//         } else {
+//             pattern += "1 ";
+//         }
+            
+//         }
+
+//     }
+//     console.log(pattern);
+//
+
+
+
+
+
+
+
+
+
+// let n = 5;
+
+// for (let i = n; i>=0; i--) {
+//     let str = '';
+
+//     for (let j = 1; j <= i; j++) {
+//         str += '*';
+//     }
+//     for (let j = 0; j < (n - i) * 2; j++) {
+//         str += ' ';
+//     }
+//     for (let j = 1; j <= i; j++) {
+//         str += '*';
+//     }
+
+//     console.log(str);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let n = 5;
+
+// for (let i = n; i>=0; i--) {
+//     let str = '';
+
+//     for (let j = 1; j <= i; j++) {
+//         str += '*';
+//     }
+//     for (let j = 0; j < (n - i) * 2; j++) {
+//         str += ' ';
+//     }
+//     for (let j = 1; j <= i; j++) {
+//         str += '*';
+//     }
+//     console.log(str);
+// }
+// for(let i = 1; i <=n ; i++){
+//     str = '';
+//     for(let j = 1; j <= i; j++){
+//         str += '*';
+//     }
+//     for(let j = 0; j < (n-i) * 2; j++){
+//         str += ' ';
+//     }
+//     for(let j =1; j <=i; j++ ){
+//         str += '*';
+//     }
+//     console.log(str);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
